58f81909efba0b0c0bd773b52763287bfe9c3a77
Daily-manjoo <yuslz35@gmail.com>
2024-03-27 23:47:16 +0900

    State로 업데이트

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
-import Player from "./components/Player"
+import Player from "./components/Player";
+import GameBoard from "./components/GameBoard";
 
 function App() {
-  
-
   return (
     <main>
       <div id="game-container">
@@ -10,13 +9,11 @@ function App() {
           <Player initialName="Player 1" symbol="X" />
           <Player initialName="Player 2" symbol="O" />
         </ol>
-
-        GAME BOARD
+        <GameBoard />
       </div>
-
       LOG
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/GameBoard.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
+export default function GameBoard() {
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  function handleSelectSquare(rowIndex, colIndex) {
+    setGameBoard((prev) => {
+      const updatedBoard = [...prev.map((innerArray) => [...innerArray])];
+      updatedBoard[rowIndex][colIndex] = "X"; //어느 요소가 이 중첩 배열에 있는지 알 수 있음
+      return updatedBoard;
+    });
+  }
+
+  return (
+    <ol id="game-board">
+      {gameBoard.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, colIndex) => (
+              <li key={colIndex}>
+                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>
+                  {playerSymbol}
+                </button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
+}
